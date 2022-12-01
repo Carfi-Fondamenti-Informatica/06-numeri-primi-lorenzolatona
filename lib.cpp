74b472eace471e36e858119656ab6b52d1537425
lorenzolatona <115403824+lorenzolatona@users.noreply.github.com>
2022-12-01 19:17:34 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,18 +1,22 @@
 //
-// Created by Lorenzo Latona on 01/12/2022.
+// Created by Lorenzo Latona on 01/12/2022
 //
 
 
 bool numero_primo(int f,int g){
     f--;
-    if (f>1){
-        if (g%f==0) {
+    if(f>1){
+        if(g%f==0){
             return false;
         }else{
             numero_primo(f,g);
         }
-    }else {
+    }else{
         return true;
     }
 }
+        
+            
+
+
 
