d5162755e9aeddb1ad3da517c125282e365d233a
lorenzolatona <115403824+lorenzolatona@users.noreply.github.com>
2022-12-01 19:22:21 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,8 +1,8 @@
 //
-// Created by Lorenzo Latona Schettini on 01/12/2022
+// Created by Lorenzo Latona on 01/12/2022
 //
 
-#ifndef ESERCITAZIONE6
-#define ESERCITAZIONE6
-bool numero_primo (int,int);
-#endif //ESERCITAZIONE6_LIB_H
+#ifndef ES6
+#define ES6
+bool numero_primo(int,int);
+#endif // ES6_LIB_H
