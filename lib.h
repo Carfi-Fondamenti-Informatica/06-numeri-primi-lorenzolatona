95419a7d9a9f8d71e63c60aa9a486e2cdc7d5f17
lorenzolatona <115403824+lorenzolatona@users.noreply.github.com>
2022-12-01 19:00:07 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -2,7 +2,7 @@
 // Created by Lorenzo Latona Schettini on 01/12/2022
 //
 
-#ifndef ESERCITAZIONE6_LIB_H
-#define ESERCITAZIONE6_LIB_H
-bool numeroprimo(int x);
-#endif// ESERCITAZIONE6_LIB_H
+#ifndef ESERCITAZIONE6
+#define ESERCITAZIONE6
+bool numero_primo (int,int);
+#endif //ESERCITAZIONE6_LIB_H
