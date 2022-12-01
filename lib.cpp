45efa894073fe8c065a05f87621e352411b4a621
lorenzolatona <115403824+lorenzolatona@users.noreply.github.com>
2022-12-01 19:19:13 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -3,13 +3,13 @@
 //
 
 
-bool numero_primo(int f,int g){
-    f--;
-    if(f>1){
-        if(g%f==0){
+bool numero_primo(int x,int y){
+    x--;
+    if(x>1){
+        if(y%x==0){
             return false;
         }else{
-            numero_primo(f,g);
+            numero_primo(x,y);
         }
     }else{
         return true;
